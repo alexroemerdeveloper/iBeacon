e68a64302ba8ba0d878107af58be0592fa87acb1
Alexander Römer <alex@roemer.design>
2019-12-21 16:50:35 +0100

    Initial Commit

---
+++ iBeacon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iBeacon
+//
+//  Created by Alexander Römer on 21.12.19.
+//  Copyright © 2019 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
